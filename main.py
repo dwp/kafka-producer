7382ddc2bbc4554c6aa000ff4d12fbc7e63da0d8
Matt Burgess <matthewburgess@digital.uc.dwp.gov.uk>
2019-03-22 14:50:00 +0000

    Initial attempt at implementing a Kafka producer

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import sys
+#!/usr/bin/env python3
+
 import argparse
 import boto3
+import json
 import logging
 import os
-import json
+import sys
+
+from kafka import KafkaProducer
 
 # Initialise logging
 logger = logging.getLogger(__name__)
@@ -25,6 +29,9 @@ def get_parameters():
     # Parse command line inputs and set defaults
     parser.add_argument("--aws-profile", default="default")
     parser.add_argument("--aws-region", default="eu-west-2")
+    parser.add_argument("--kafka-bootstrap-servers")
+    parser.add_argument("--s3-bucket")
+    parser.add_argument("--s3-bucket-prefix")
 
     _args = parser.parse_args()
 
@@ -35,15 +42,55 @@ def get_parameters():
     if "AWS_REGION" in os.environ:
         _args.aws_region = os.environ["AWS_REGION"]
 
+    if "KAFKA_BOOTSTRAP_SERVERS" in os.environ:
+        _args.kafka_bootstrap_servers = os.environ["KAFKA_BOOTSTRAP_SERVERS"]
+
+    if "S3_BUCKET" in os.environ:
+        _args.s3_bucket = os.environ["S3_BUCKET"]
+
+    if "S3_BUCKET_PREFIX" in os.environ:
+        _args.s3_bucket_prefix = os.environ["S3_BUCKET_PREFIX"]
+
+    missing_args = False
+    if not _args.kafka_bootstrap_servers:
+        missing_args = True
+        logger.error(
+            "Missing required argument: --kafka_bootstrap_servers (KAFKA_BOOTSTRAP_SERVERS)"
+        )
+
+    if not _args.s3_bucket:
+        missing_args = True
+        logger.error("Missing required argument: --s3-bucket (S3_BUCKET)")
+
+    if not _args.s3_bucket_prefix:
+        missing_args = True
+        logger.error("Missing required argument: --s3-bucket-prefix (S3_BUCKET_PREFIX)")
+
+    if missing_args:
+        raise argparse.ArgumentError(None, "Missing required argument(s)")
+
     return _args
 
 
 def handler(event, context):
-    args = get_parameters()
+    try:
+        args = get_parameters()
+    except argparse.ArgumentError as e:
+        raise
+
     try:
         produce_kafka_messages(event, args)
     except KeyError as key_name:
         logger.error(f"Key: {key_name} is required in payload")
+
+
+def get_s3_keys(bucket, prefix):
+    s3_paginator = boto3.client("s3").get_paginator("list_objects_v2")
+
+    logger.debug(f"Processing prefix: {prefix}")
+    for page in s3_paginator.paginate(Bucket=bucket, Prefix=prefix):
+        for content in page.get("Contents", ()):
+            yield content["Key"]
 
 
 def produce_kafka_messages(event, args):
@@ -60,6 +107,8 @@ def produce_kafka_messages(event, args):
 
     message = json.loads(event["Records"][0]["Sns"]["Message"])
     logger.debug(message)
+    if "fixture_data" not in message:
+        raise KeyError("fixture_data")
 
     # {
     #     "job_id": "aws-ingest_upload-fixture-data-dev_13",
@@ -74,6 +123,27 @@ def produce_kafka_messages(event, args):
     update_job_status(message["job_id"], "RUNNING")
 
     # Process each fixture data dir
+    producer = KafkaProducer(bootstrap_servers=args.kafka_bootstrap_servers)
+    s3_client = boto3.client("s3")
+    for prefix in message["fixture_data"]:
+        for s3_key in get_s3_keys(args.s3_bucket, prefix):
+            logger.debug(f"Processing key: {s3_key}")
+            line_no = 0
+            for line in (
+                s3_client.get_object(Bucket=args.s3_bucket, Key=s3_key)["Body"]
+                .read()
+                .splitlines()
+            ):
+                line_no += 1
+                try:
+                    json.loads(line)
+                except json.JSONDecodeError as e:
+                    logger.error(
+                        f"line {line_no} of {key} contains invalid JSON data: {e.msg}"
+                    )
+                    continue
+
+                producer.send(line)
 
     # Update status on dynamo db record
     update_job_status(message["job_id"], "COMPLETE")
@@ -97,6 +167,4 @@ if __name__ == "__main__":
         json_content = json.loads(open("event.json", "r").read())
         handler(json_content, None)
     except Exception as e:
-        logger.error("Unexpected error occurred")
         logger.error(e)
-        raise e
