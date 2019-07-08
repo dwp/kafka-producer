f69f98710b9d47b50518aed70fd0f169ab505095
mark matthews <mark.matthews1@dwp.gov.uk>
2019-07-08 13:25:39 +0100

    treat each s3 file as its own json file

--- main.py
+++ main.py
@@ -112,23 +112,23 @@ def produce_kafka_messages(bucket, job_id, fixture_data, args):
     s3_client = boto3.client("s3")
     for s3_key in fixture_data:
         logger.debug(f"Processing key: {s3_key}")
-        line_no = 0
-        for line in (
+        file_no = 0
+        for file in (
             s3_client.get_object(Bucket=bucket, Key=s3_key)["Body"]
             .read()
         ):
-            line_no += 1
+            file_no += 1
             try:
-                data = json.loads(line)
+                data = json.loads(file)
                 db = data["message"]["db"] if "db" in data["message"] else "missingDb"
                 collection = data["message"]["collection"] if "collection" in data["message"] else "missingCollection"
                 topic_name = f"{args.topic_prefix}{job_id}_{db}.{collection}"
             except json.JSONDecodeError as err:
                 logger.error(
-                    f"line {line_no} of {s3_key} contains invalid JSON data: {err.msg}"
+                    f"line {file_no} of {s3_key} contains invalid JSON data: {err.msg}"
                 )
                 continue
-            producer.send(topic_name, line)
+            producer.send(topic_name, file)
             producer.flush()
 
 
